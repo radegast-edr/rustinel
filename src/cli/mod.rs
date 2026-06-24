8d9adc78a45e2ad7bdad35f92b3c86e69fd864cb
Adam Hlavacek <git@adamhlavacek.com>
2026-06-24 16:31:19 +0200

    patch: allow building custom patches

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -2,7 +2,7 @@ pub mod reference;
 
 #[derive(clap::Parser)]
 #[command(name = "rustinel")]
-#[command(version)]
+#[command(version = concat!(env!("CARGO_PKG_VERSION"), "r", env!("RADEGAST_PATCH_VERSION")))]
 #[command(about = "High-Performance Rust EDR", long_about = None)]
 pub struct Cli {
     #[command(subcommand)]
